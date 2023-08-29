40152effedcaad6076a76f2b62f7d06905ad9838
VladRaFeed <122728801+VladRaFeed@users.noreply.github.com>
2023-08-29 10:09:10 +0300

    fix#5

--- src/servises/imagesApi.jsx
+++ src/servises/imagesApi.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '39014637-350db469d238078cedfe93bb7';
 
 // https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
